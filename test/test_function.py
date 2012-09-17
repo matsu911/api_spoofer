d42f5e04c88aac119d537e884c0a6205f1e68001
Shigeaki Matsumura <matsu911@gmail.com>
2012-09-17 20:48:29 +0900

    refactoring

---
+++ test/test_function.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from api_spoofer import extract_function_declares, get_symbol_names
+
+class TestFunctions(unittest.TestCase):
+    def test_extract_function_declares(self):
+        funcs = extract_function_declares('/usr/include/stdio.h')
+        self.assertTrue(['size_t', 'fread', ['void *__restrict __ptr', 'size_t __size', 'size_t __n', 'FILE *__restrict __stream']] in 
+                        funcs)
+        self.assertTrue(['size_t', 'fwrite', ['__const void *__restrict __ptr', 'size_t __size', 'size_t __n', 'FILE *__restrict __s']] in 
+                        funcs)
+        
+    def test_get_symbol_names(self):
+        syms = get_symbol_names('/bin/date')
+        self.assertTrue('clock_settime' in syms)
