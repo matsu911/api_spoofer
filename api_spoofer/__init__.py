f2eec886243ebe7a161218705eee15e1c00f485c
Shigeaki Matsumura <matsu911@gmail.com>
2012-09-18 00:53:25 +0900

    added command api_spoofer

--- api_spoofer/__init__.py
+++ api_spoofer/__init__.py
@@ -3,3 +3,41 @@
 
 from extract import extract_function_declares, preprocess_file
 from pyELF import get_symbol_names
+
+import sys
+sys.path.append('..')
+from api_spoofer import extract_function_declares, get_symbol_names
+import os
+
+def main():
+    header_files = ['/usr/include/stdio.h', '/usr/include/stdlib.h', '/usr/include/time.h']
+    funcs = extract_function_declares(*header_files)
+    syms = get_symbol_names('/bin/date')
+
+    print "#define _GNU_SOURCE 1"
+    print "#include <dlfcn.h>"
+    for header in map(os.path.basename, header_files):
+        print "#include <%s>" % header
+    print
+
+    for ret_type, fun_name, args in funcs:
+        args = filter(lambda x: x[0] != 'void', args)
+        if fun_name in syms:
+            for i in xrange(0, len(args)):
+                if not args[i][1]:
+                    args[i][1] = 'arg%d' % i
+            if ret_type == 'void':
+                print """void %s(%s)
+{
+  typedef void (*ftype)(%s);
+  ((ftype)dlsym(RTLD_NEXT, "%s"))(%s);
+}
+""" % (fun_name, ", ".join([" ".join(x) for x in args]), ", ".join([x[0] for x in args]), fun_name, ", ".join([x[1] for x in args]))
+            else:
+                print """%s %s(%s)
+{
+  typedef %s (*ftype)(%s);
+  return ((ftype)dlsym(RTLD_NEXT, "%s"))(%s);
+}
+""" % (ret_type, fun_name, ", ".join([" ".join(x) for x in args]), ret_type, ", ".join([x[0] for x in args]), fun_name, ", ".join([x[1] for x in args]))
+
--- setup.py
+++ setup.py
@@ -10,5 +10,15 @@ sys.path.append('./test')
 setup(name = "API Spoofer",
       version = "0.0.1",
       packages = find_packages(),
-      test_suite = 'test_main.suite'
+      author = "Shigeaki Matsumura",
+      author_email = "matsu911@gmail.com",
+      description = "API Spoofing Tool",
+      url = "https://github.com/matsu911/api_spoofer",
+      license = "GPL",
+      long_description = "README.md",
+      keywords = "API spoofing",
+      test_suite = 'test_main.suite',
+      entry_points = {
+        'console_scripts' : "api_spoofer = api_spoofer:main"
+        }
       )
